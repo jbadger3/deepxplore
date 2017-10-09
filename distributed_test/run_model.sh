7467b741a408527b6272420e07d169f2c8e21a81
Ubuntu <ubuntu@local.com>
2017-10-09 02:53:22 +0000

    fix bugs for running on cluster

--- distributed_test/run_model.sh
+++ distributed_test/run_model.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-script=$1
-num_steps=$2
+num_steps=$1
 
-#start param server on vm-3-2 vm-3-1 will be chief
-ssh vm-3-2 "'python $script --job_name ps --task_index 0 --num_steps $num_steps'"
-for vm in 2 3 4 5;
+#start param server on vm-3-5 vm-3-1 will be chief
+ssh vm-3-5 python ~/project/cs744_project_d3/distributed_test/distributed_test.py --job_name ps --task_index 0 --num_steps $num_steps &
+for vm in 1 2 3 4;
 do
   let task="$vm - 1"
-  ssh vm-3-$vm "'python $script --job_name worker --task_index $task --num_steps $num_steps'"
+  ssh "vm-3-$vm" python ~/project/cs744_project_d3/distributed_test/distributed_test.py --job_name worker --task_index $task --num_steps $num_steps &
 done
-python $script --job_name worker --task_index 0 --num_steps $num_steps
 # start cheif on vm-3-1
+#python ~/project/cs744_project_d3/distributed_test/distributed_test.py --job_name worker --task_index 0 --num_steps $num_steps
