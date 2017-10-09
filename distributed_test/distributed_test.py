c2cb675e43baae05c1fe938976ea2c7e0b1e94c0
Ubuntu <ubuntu@local.com>
2017-10-09 16:54:21 +0000

    squash attempt

--- distributed_test/distributed_test.py
+++ distributed_test/distributed_test.py
@@ -30,7 +30,7 @@ def cluster_spec_dict(should_run_local):
         return {'local':['localhost:2222','localhost:2223']}
     else:
     #host parameter server on VM-3-1 and works on VM-3-2 to VM-3-5
-        return {'ps':['10.254.0.36:2222'],'worker':['10.254.0.32:2223', '10.254.0.33:2224','10.254.0.34:2225','10.254.0.35:2226']}
+        return {'ps':['10.254.0.36:2222'],'worker':['10.254.0.32:2222', '10.254.0.33:2222','10.254.0.34:2222','10.254.0.35:2222']}
 
 def main(_):
 
