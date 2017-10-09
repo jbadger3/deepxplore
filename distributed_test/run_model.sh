49f8c1d4afdf6ef83929bbb143e314c1dd073f40
Ubuntu <ubuntu@vm-3-2.com>
2017-10-09 14:42:42 +0000

    squash attempt

--- distributed_test/run_model.sh
+++ distributed_test/run_model.sh
@@ -9,5 +9,5 @@ python ~/project/cs744_project_d3/distributed_test/distributed_test.py --job_nam
 for vm in 2 3 4;
 do
   let task="$vm - 1"
-  ssh "vm-3-$vm" python ~/project/cs744_project_d3/distributed_test/distributed_test.py --job_name worker --task_index $task --num_steps $num_steps &
+  ssh vm-3-$vm python ~/project/cs744_project_d3/distributed_test/distributed_test.py --job_name worker --task_index $task --num_steps $num_steps &
 done
