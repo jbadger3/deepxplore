07adaa53a1bad173b2601383ebd984691b3c4d2b
Ubuntu <ubuntu@local.com>
2017-10-10 18:12:59 +0000

    fixed script to run properly.  no longer exits script from exit status >0

--- kill_all_python_tasks.sh
+++ kill_all_python_tasks.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #to view all listening ports
 #pdsh netstat -tulpn | grep LISTEN
-pdsh pkill -f python
+for vm in 2 3 4 5
+do
+  echo "killing python processes in vm-3-$vm"
+  ssh "vm-3-$vm" "pkill -f python && exit" || true
+done
+echo "killing python processes in vm-3-1"
+pkill -f python
+#pdsh pkill -f python
