f502ae6e1ee4187181e3a73ab9d37bb42418aa29
Jon Badger <jonathancbadger@gmail.com>
2017-11-15 10:20:13 -0600

    1.0 working version

--- deep_xplore.py
+++ deep_xplore.py
@@ -19,15 +19,8 @@ def ensure_dir_exists(directory):
         except FileNotFoundError as err:
             print('Could not create directory: {}.\nError was: {}\nCheck directory structure.'.format(directory,err))
 
-def meta_file_at_path(path):
-    files = os.listdir(path)
-    for file_ in files:
-        if file_.endswith('.meta'):
-            meta_file = os.path.join(path,file_)
-    assert meta_file, 'No metafile found in {}'.format(path)
-    return meta_file
 
-def create_models(config):
+def create_models(config,x):
 
     """Loads pretrained models to the default object graph for use with DeepXplore.  Also performs the following.
     1.  Grab handles to the model input, output, and intermediate output layer tensors
@@ -36,16 +29,17 @@ def create_models(config):
     models = {}
     model_locations = config['models']
     for model_name, model_info in model_locations.items():
-        meta_file = meta_file_at_path(model_info['model_dir'])
-        saver = tf.train.import_meta_graph(meta_file)
-        x = graph.get_tensor_by_name(model_info['inputs'])
-        y = graph.get_tensor_by_name(model_info['outputs'])
-        y_labels = graph.get_tensor_by_name(model_info['labels'])
-        loss = graph.get_tensor_by_name(model_info['loss'])
+        import_name = model_info['import_name']
+        #import the module
+        module = importlib.import_module(import_name)
+        y = module.model(x)
+        vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=model_name)
+        var_dict = {_var.name.split(':')[0]:_var for _var in vars_list}
+        saver = tf.train.Saver(var_list=var_dict)
         neurons = graph.get_collection('neurons',scope=model_name)
         coverage_dict = create_coverage_dict(neurons)
         #class_prob = tf.reduce_max(y,axis =1)
-        models[model_name] = {'saver':saver,'inputs':x,'outputs':y,'labels':y_labels,'neurons':neurons,'loss':loss,'coverage_dict':coverage_dict}
+        models[model_name] = {'saver':saver,'outputs':y,'neurons':neurons,'coverage_dict':coverage_dict}
 
     return models
 
@@ -73,17 +67,17 @@ def choose_random_neuron(coverage_dict):
         layer_tensor, tensor_index = random.choice(coverage_dict.keys())
     return layer_tensor, tensor_index
 
-def neuron_fired(model_dict,layer, tensor_index,input_data, threshold):
-    x = model_dict['inputs']
+def neuron_fired(model_dict,layer, tensor_index,x,input_data, threshold):
     layer_output = layer.eval(feed_dict={x:input_data})
     max_layer_out = np.max(layer_output,axis=0)
     return max_layer_out[tensor_index] > threshold
 
-def neuron_val(model_dict,layer,tensor_index,input_data):
-    x = model_dict['inputs']
-    layer_output = layer.eval(feed_dict={x:input_data})
-    avg_layer_out = np.mean(layer_output,axis=0)
-    return avg_layer_out[tensor_index]
+def neuron(layer,tensor_index,input_data):
+    batch_size = input_data.shape[0]
+    tensor_list = list(tensor_index)
+    tensor_list.insert(0,batch_size - 1)
+    neuron = layer[tensor_list]
+    return neuron
 
 def create_coverage_dict(neurons):
     coverage_dict = OrderedDict()
@@ -103,13 +97,12 @@ def create_layer_coverage(layer,coverage_dict,tensor_indices=None, tensor_level=
         else:
             create_layer_coverage(layer,coverage_dict,tensor_indices=tensor_indices,tensor_level=tensor_level+1)
 
-def update_coverage(input_data, models, threshold):
+def update_coverage(x,gen_img, models, threshold):
     for model_name, model_dict in models.items():
         neurons = model_dict['neurons']
-        x = model_dict['inputs']
         coverage_dict = model_dict['coverage_dict']
         for layer in neurons:
-            layer_output = layer.eval(feed_dict={x:input_data})
+            layer_output = layer.eval(feed_dict={x:gen_img})
             #take max accross dimension 0
             max_layer_out = np.max(layer_output,axis =0)
             above_threshold_mask = np.greater(max_layer_out,threshold)
@@ -119,82 +112,89 @@ def update_coverage(input_data, models, threshold):
                 coverage_dict[(layer,tensor_index)]=it[0]
                 it.iternext()
 
-def constraint_light(gradients):
+def constrain_light(gradients):
     new_grads = np.ones_like(gradients)
     grad_mean = np.mean(gradients)
     return grad_mean * new_grads
 
-def model_predictions(models, gen_img):
+
+def model_predictions(models,x, gen_img):
     predictions = []
+    pred_model_labels = []
     for model_name,model_dict in models.items():
-        x = model_dict['inputs']
         outputs = model_dict['outputs']
         prediction = np.argmax(outputs.eval(feed_dict={x:gen_img}),axis=1)
         predictions.append(prediction[0])
-    return predictions
+        pred_model_labels.append(model_name)
+    return predictions, pred_model_labels
 
 def main(_):
-    models = load_models(config)
+    x = tf.placeholder(tf.float32, shape=[None, 784],name='inputs')
+    models = create_models(config,x)
     target_model = config['target_model']
+    threshold = config['threshold']
+    transformation = config['transformation']
+    step_size = config['step_size']
     lambda1 = tf.constant(config['lambda1'])
     lambda2 = tf.constant(config['lambda2'])
-    orig_label = tf.placeholder(tf.float32)
-    input_tensor = tf.placeholder(tf.float32,shape=(None,784))
 
     #define all computed losses and gradients before initialization of the graph
     non_target_models = set(models.keys())
     non_target_models.difference_update(set([target_model]))
-    target_model_loss = tf.multiply(tf.negative(models[target_model]['loss']),lambda1)
-    non_target_losses = tf.add_n([models[model]['loss'] for model in non_target_models])
+    target_model_loss = tf.multiply(tf.negative(models[target_model]['outputs']),lambda1)
+    non_target_losses = tf.add_n([models[model]['outputs'] for model in non_target_models])
     obj1_loss = tf.add(non_target_losses,target_model_loss)
-    neuron_sum = tf.placeholder(tf.float32)
-    obj2_loss = tf.reduce_mean(tf.add(obj1_loss,tf.multiply(neuron_sum,lambda2)))
-    grads = tf.gradients(obj2_loss,input_tensor)
 
-
-
-    with tf.Session() as sess:
-        #load trained weights for all graphs
-        for model_name,model_dict in models.items():
-            vars_list = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=model_name)
-            var_dict = {avar.name:avar for avar in vars_list}
-            saver = tf.train.Saver(var_names)
-            saver = model_dict['saver']
-            model_dir = config['models'][model_name]['model_dir']
-            saver.restore(sess,tf.train.latest_checkpoint(model_dir))
-
-        for seed in range(args.seeds):
-            gen_img = mnist.test.next_batch(args.batch_size)[0]
+        for seed in range(2000):
+            #gen_img = mnist.test.next_batch(args.batch_size)[0]
             gen_img = mnist.test.next_batch(1)[0]
             orig_img = gen_img.copy()
-            predictions = model_predictions(models, gen_img)
 
-            if not len(set(predictions)) == 1:
-                #classification is already different in the networks.
-                #in original DeepXplore these images are added to the difference inducing list.  We will skip and focus on images created by transformation
-                continue
-            shared_label = np.zeros((1,10))
-            shared_label[(0,predictions[0])] = 1
-            #choose a random neuron from each network currently not covered
             neurons_to_cover = []
             for model_name,model_dict in models.items():
                 coverage_dict = model_dict['coverage_dict']
                 layer, tensor_index = choose_random_neuron(coverage_dict)
-                neurons_to_cover.append((model_name,layer,tensor_index))
+                neurons_to_cover.append(neuron(layer,tensor_index,gen_img))
+            neuron_sum = tf.add_n(neurons_to_cover)
+            obj2_loss = tf.reduce_mean(tf.add(obj1_loss,tf.multiply(neuron_sum,lambda2)))
+            grads = tf.gradients(obj2_loss,x)[0]
+            with tf.Session() as sess:
+                #load trained weights for all graphs
+                for model_name,model_dict in models.items():
+                    saver = model_dict['saver']
+                    model_dir = config['models'][model_name]['model_dir']
+                    saver.restore(sess,tf.train.latest_checkpoint(model_dir))
 
-            #run gradient ascent for 20 steps
-            for step in range(0,20):
-                neuron_vals = []
-                for model_name, layer, tensor_index in neurons_to_cover:
-                    model_dict = models[model_name]
-                    neuron_vals.append(neuron_val(model_dict,layer,tensor_index,gen_img))
-                _neuron_sum = np.sum(neuron_vals)
-                feed_dict = {model['inputs']:gen_img for model in models.values()}
-                for model_dict in models.values():
-                    feed_dict[model_dict['labels']] = shared_label
-                feed_dict[neuron_sum] = _neuron_sum
-                feed_dict[input_tensor] = gen_img
-                grad_vals = grads.eval(feed_dict=feed_dict)
+
+                predictions, pred_model_labels = model_predictions(models,x, gen_img)
+                if not len(set(predictions)) == 1:
+                    #classification is already different in the networks.
+                    #in original DeepXplore these images are added to the difference inducing list.  We will skip and focus on images created by transformation
+                    continue
+                #shared_label = np.zeros((1,10))
+                #shared_label[(0,predictions[0])] = 1
+                shared_label = predictions[0]
+                #choose a random neuron from each network currently not covered
+
+
+                #run gradient ascent for 20 steps
+                for step in range(0,20):
+                    gradients = grads.eval(feed_dict={x:gen_img})
+                    if transformation == 'light':
+                        gradients = constrain_light(gradients)
+                    gen_img += gradients*step_size
+                    gen_img = np.clip(gen_img,0,1)
+                    predictions, pred_model_labels = model_predictions(models,x,gen_img)
+                    if not len(set(predictions)) == 1:
+                        update_coverage(x,gen_img, models, threshold)
+                        final_gen_img = raw_data_to_image(gen_img)
+                        final_orig_img = raw_data_to_image(orig_img)
+                        new_label = list(set(predictions).difference(set([shared_label])))[]
+                        final_gen_img_name = '{}_seed_{}_label{}_to_{}.png'.format(target_model,seed,shared_label,new_label)
+                        final_orig_img_name = '{}_seed_{}_orig.png'.format(target_model,seed)
+                        imsave(os.path.join(out_dir,final_gen_img_name),final_gen_img)
+                        imsave(os.path.join(out_dir,final_orig_img_name),final_orig_img)
+                        break
 
 
 
